3433d754b758512f8ecb5c6b0d5b56782b303f91
creepid <mikhail.complete@gmail.com>
2018-05-05 20:41:57 +0300

    xss search add

---
+++ xss/search.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>XSS</title>
+    <script src="jquery-3.1.0.min.js"></script>
+	<script>
+	var output = "";
+	<?php
+	//?q=<script>alert(1)</script> -> return output scripting
+	//?q=<script>alert(1)</sc"%2B"ript> -> success
+	//
+	if (isset($_GET['q'])) {
+ 	  printf('output = "%s";', $_GET['q']);
+	}
+	?>
+	
+	$(function()  {
+	  $("#output").html(output);
+	});
+	</script>
+  </head>
+  <body>
+    <p id="output"></p>
+  </body>
+</html>
